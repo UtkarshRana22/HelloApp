e6725df22a18f23d0d22e77c9507afb5dce81c68
UtkarshRana22 <ranautkarsh8709@gmail.com>
2026-03-30 23:40:56 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,25 +2,9 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name;
-
-        if (args.length > 0) {
-
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String arg : args) {
-                nameBuilder.append(arg).append(", ");
-            }
-
-            if (nameBuilder.length() > 0) {
-                name = nameBuilder.substring(0, nameBuilder.length() - 2);
-            } else {
-                name = "";
-            }
-
-        } else {
-            name = "World";
-        }
+        String name = (args.length > 0)
+                ? String.join(", ", args)
+                : "World";
 
         System.out.println("Hello, " + name + "!");
     }
